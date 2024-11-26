01a91ca7c473e2d9a7ce5a062a696f266c3a2ba0
gonzalo andres salas tranolao <gonzalo.salas.tr@gmail.com>
2024-11-26 17:36:44 -0300

    fin

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -2,11 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from '../screens/LoginScreen';
-import ProductListScreen from '../screens/ProductListScreen';  // Pantalla de lista de productos
-import ProductDetailScreen from '../screens/ProductDetailScreen';  // Pantalla de detalles de productos
-
-const Stack = createNativeStackNavigator();
-
+import ProductListScreen from '../screens/ProductListScreen';  
+import ProductDetailScreen from '../screens/ProductDetailScreen';                                                                     
 export default function AppNavigator() {
 
   const [isAuthenticated, setIsAuthenticated] = useState(false);
